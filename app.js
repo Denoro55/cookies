ef3ada8b3a8ca96e015462272a0cd6449a63eb55
Denis Chertenko <denis.chertenko@mail.ru>
2019-12-16 16:12:19 +0300

    feat(cookies) тестирование библиотеки

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
-/*global __dirname */
-
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+const http = require('http')
+// const path = require('path');
+// const app = express();
+const Cookies = require('cookies');
 const port = 3000;
 
-app.use('/', (req, res) => {
+// app.use('/', (req, res) => {
 
-	console.log('Cookies from client: ' + req.headers['cookie']);
+// 	const cookies = new Cookies(req, res);
 
-	// res.setHeader('Set-Cookie', 'express=4.0');
+// 	cookies.set('admin', true);
+// 	console.log(cookies.get('admin'));
 
-	// массив кук
-	res.setHeader('Set-Cookie', ['value=item', 'value2=item2']);
+// 	console.log('Cookies from client: ' + req.headers['cookie']);
 
-	// res.status(200);
+// 	// res.setHeader('Set-Cookie', 'express=4.0');
 
-	// код ответа с куками
-	// res.writeHead(200, { 'Set-Cookie': ['value=item', 'value2=item2'] } );
+// 	// массив кук
+// 	res.setHeader('Set-Cookie', ['value=item', 'value2=item2']);
 
-	res.sendFile(path.join(__dirname, 'index.html'));
+// 	// res.status(200);
 
-});
+// 	// код ответа с куками
+// 	// res.writeHead(200, { 'Set-Cookie': ['value=item', 'value2=item2'] } );
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// 	res.sendFile(path.join(__dirname, 'index.html'));
+
+// });
+
+const keys = ['keyboard cat']
+
+const server = http.createServer(function (req, res) {
+  // Create a cookies object
+  var cookies = new Cookies(req, res, { keys: keys })
+ 
+  // Get a cookie
+  var lastVisit = cookies.get('LastVisit', { signed: true })
+ 
+  // Set the cookie to a value
+  cookies.set('LastVisit', new Date().toISOString(), { signed: true })
+ 
+  if (!lastVisit) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Welcome, first time visitor!')
+  } else {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Welcome back! Nothing much changed since your last visit at ' + lastVisit + '.')
+  }
+})
+
+server.listen(port, function () {
+	console.log('Visit us at port ' + port + ' !');
+})
